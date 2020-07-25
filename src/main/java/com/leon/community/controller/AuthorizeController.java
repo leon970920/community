8743d9f9447554b30852321b3c854ee262bda01d
chengliang <chengliang@egoonet.com>
2020-07-25 16:04:18 +0800

    抽离一些属性进配置文件

--- src/main/java/com/leon/community/controller/AuthorizeController.java
+++ src/main/java/com/leon/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.leon.community.dto.AccessTokenDTO;
 import com.leon.community.dto.UserDTO;
 import com.leon.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +21,27 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    /**
+     * 注入配置参数
+     */
+    @Value(value = "${github.client_id}")
+    private String clientId;
+
+    @Value(value = "${github.client_secret}")
+    private String clientSecret;
+
+    @Value(value = "${github.redirect_uri}")
+    private String redirectUri;
+
     @RequestMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_id("61aef5a7582348c5beff");
-        accessTokenDTO.setClient_secret("e6c7ec2d11dc5a2de310f958eda1183291b487a4");
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
 
         String accessToken=githubProvider.getAccessToken(accessTokenDTO);
